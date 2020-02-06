afee9440780484ef6cabfa26f521480496e469bb
Stephan Mertens <steph_cs@gmx.de>
2020-02-06 18:54:07 +0100

    typo

--- Fensterscheiss/test_button.py
+++ Fensterscheiss/test_button.py
@@ -5,7 +5,7 @@ G.setmode(G.BCM)
 G.setwarnings(0)
 
 #G.setup(22, G.IN, pull_up_down=G.PUD_UP)
-G.setup(10, G.IN, pull_up_down=G.PUD_DOWN)
+G.setup(22, G.IN, pull_up_down=G.PUD_DOWN)
 
 if __name__ == '__main__':
     #for i in range(1,6):
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     #        sleep(0.1)
     #    print('Pushed')
     while True: # Run forever
-        if G.input(10) == G.HIGH:
+        if G.input(22) == G.HIGH:
             print("Pushed!")
     print('Test exited successfully.')
     G.cleanup()
