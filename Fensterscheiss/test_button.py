071a0349d2131ea7aa973d88ae3dc57004f059f4
Stephan Mertens <steph_cs@gmx.de>
2020-02-06 18:51:55 +0100

    Button neu verkabelt

--- Fensterscheiss/test_button.py
+++ Fensterscheiss/test_button.py
@@ -4,14 +4,17 @@ from time import sleep
 G.setmode(G.BCM)
 G.setwarnings(0)
 
-G.setup(22, G.IN, pull_up_down=G.PUD_UP)
-
+#G.setup(22, G.IN, pull_up_down=G.PUD_UP)
+G.setup(10, G.IN, pull_up_down=G.PUD_DOWN)
 
 if __name__ == '__main__':
-    for i in range(1,6):
-        while G.wait_for_edge(22, G.FALLING):
-            sleep(0.1)
-        print('Pushed')
+    #for i in range(1,6):
+    #    while G.wait_for_edge(22, G.FALLING):
+    #        sleep(0.1)
+    #    print('Pushed')
+    while True: # Run forever
+        if G.input(10) == G.HIGH:
+            print("Pushed!")
     print('Test exited successfully.')
     G.cleanup()
     exit()
