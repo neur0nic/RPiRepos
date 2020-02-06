2d25171d7599d6b2ebde9a1e30d354016f6e8e44
Stephan Mertens <steph_cs@gmx.de>
2020-02-06 19:51:41 +0100

    And another try

--- Fensterscheiss/test_button.py
+++ Fensterscheiss/test_button.py
@@ -1,20 +1,14 @@
 import RPi.GPIO as G
-from time import sleep
 
 G.setmode(G.BCM)
 G.setwarnings(0)
 
-#G.setup(22, G.IN, pull_up_down=G.PUD_UP)
 G.setup(22, G.IN, pull_up_down=G.PUD_DOWN)
 
 if __name__ == '__main__':
-    #for i in range(1,6):
-    #    while G.wait_for_edge(22, G.FALLING):
-    #        sleep(0.1)
-    #    print('Pushed')
-    while True: # Run forever
-        if G.input(22) == G.HIGH:
-            print("Pushed!")
+    while True:
+        if G.input(15) == G.HIGH:
+            print("Button was pushed!")
     print('Test exited successfully.')
     G.cleanup()
     exit()
