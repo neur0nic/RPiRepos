7d7ca8d8ead99cd2a5833a27947c5107aa5e0129
Stephan Mertens <steph_cs@gmx.de>
2020-02-06 20:10:24 +0100

    And another one.

--- Fensterscheiss/test_button.py
+++ Fensterscheiss/test_button.py
@@ -5,9 +5,19 @@ G.setwarnings(0)
 
 G.setup(22, G.IN, pull_up_down=G.PUD_DOWN)
 
+
+def button():
+    while not G.input(22):
+        pass
+    print('Pushed')
+    return True
+
+
 if __name__ == '__main__':
-    while True:
-        if G.input(22) == G.HIGH:
-            print("Button was pushed!")
+    button()
+    #while True:
+    #    if G.input(22) == G.HIGH:
+    #        print("Button was pushed!")
+    print('Test exited successfully.')
     G.cleanup()
     exit()
